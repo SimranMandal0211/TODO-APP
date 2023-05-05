06c7e5e83c91028bea8b534e54f0f5002ecc0724
Simran Mandal <simranmandal0211@gmail.com>
2023-05-05 17:34:51 +0530

    Add BgColor to Category

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,1 +1,34 @@
-console.log('hello home.js !!');
+console.log('Hello JS !!');
+
+let colorBtn = document.querySelector('.addTasks button');
+
+let x = colorBtn.innerText;
+switch(x){
+  case 'personal' :
+      colorBtn.style.backgroundColor = "green";
+      console.log('personal');
+    break;
+  case 'work' :
+    colorBtn.style.backgroundColor = "blue";
+    console.log('work');
+    break;
+  case 'school' :
+    colorBtn.style.backgroundColor = "orangered";
+    console.log('school');
+      break;
+  case 'cleaning' :
+    colorBtn.style.backgroundColor = "tomato";
+    console.log('cleaning');
+      break;
+  case 'other' :
+    colorBtn.style.backgroundColor = "olive";
+    console.log('other');
+      break;
+  default : 
+    console.log('default');
+}
+
+
+console.log('x: ',x);
+
+console.log('**End**');
